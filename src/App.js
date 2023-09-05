9d3a3c639178b8bf25a44b1110418a9f7914c994
Xin <xinwen@mit.edu>
2023-09-04 21:21:14 -0600

    add new page for compfab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from './pages/index';
+import CompFab from './pages/compfab';
 import './styles/styles.scss';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<MainPage/>}/>
+        <Route exact path="/compfab" element={<CompFab/>}/>
       </Routes>
     </Router>
   );
---
+++ src/pages/compfab.js
@@ -0,0 +1,16 @@
+import '../styles/styles.scss';
+
+function CompFab() {
+  return (
+    <div className="project-pages">
+      <div className="header">
+        ATLS 5519 Computationl Fabrication
+      </div>
+      <div className="paragraph">
+        This is the documentation site for ATLS 5519 Computationl Fabrication.
+      </div>
+    </div>
+  );
+}
+
+export default CompFab;
