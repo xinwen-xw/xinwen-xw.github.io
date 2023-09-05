060e37f057a9f490f9eca3efe84775181aeb0321
Xin <xinwen@mit.edu>
2023-09-04 21:57:18 -0600

    try hashroute

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route, HashRouter } from "react-router-dom";
 import MainPage from './pages/index';
 import CompFab from './pages/compfab';
 import './styles/styles.scss';
 
 function App() {
   return (
-    <Router>
+
+    <HashRouter>
       <Routes>
         <Route exact path="/" element={<MainPage/>}/>
         <Route exact path="/compfab" element={<CompFab/>}/>
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
